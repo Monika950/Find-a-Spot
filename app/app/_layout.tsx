530fde1d52cd3c6701c4471c88a7e5ad545ba06b
Daniel Stefanov <97540472+ludsmotika@users.noreply.github.com>
2024-11-24 11:38:43 +0200

    jlhhuk

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,39 +1,43 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "../contexts/AuthContext";
+import { ParkingProvider } from "../contexts/ParkingContext";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
-    return (
-        <AuthProvider>
-            <GestureHandlerRootView style={{ flex: 1 }}>
-                <Stack>
-                    <Stack.Screen
-                        name="index"
-                        options={{
-                            headerShown: false,
-                            statusBarTranslucent: true,
-                            navigationBarHidden: true,
-                        }}
-                    />
-                    <Stack.Screen
-                        name="auth"
-                        options={{
-                            headerShown: false,
-                            statusBarTranslucent: true,
-                            navigationBarHidden: true,
-                        }}
-                    />
-                    <Stack.Screen
-                        name="map1"
-                        options={{
-                            headerShown: false,
-                            statusBarTranslucent: true,
-                            navigationBarHidden: true,
-                        }}
-                    />
-                    
-                </Stack>
-            </GestureHandlerRootView>
-        </AuthProvider>
-    );
+  return (
+    <ParkingProvider >
+
+      <AuthProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Stack>
+            <Stack.Screen
+              name="index"
+              options={{
+                headerShown: false,
+                statusBarTranslucent: true,
+                navigationBarHidden: true,
+              }}
+            />
+            <Stack.Screen
+              name="auth"
+              options={{
+                headerShown: false,
+                statusBarTranslucent: true,
+                navigationBarHidden: true,
+              }}
+            />
+            <Stack.Screen
+              name="map1"
+              options={{
+                headerShown: false,
+                statusBarTranslucent: true,
+                navigationBarHidden: true,
+              }}
+            />
+
+          </Stack>
+        </GestureHandlerRootView>
+      </AuthProvider>
+    </ParkingProvider >
+  );
 }
